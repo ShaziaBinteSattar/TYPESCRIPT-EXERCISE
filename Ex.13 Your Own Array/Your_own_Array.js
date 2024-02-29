b329f7aa4ab8ae80f7f57a85cde3caba4be81f62
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:24:20 +0500

    Add files via upload

---
+++ Ex.13 Your Own Array/Your_own_Array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['Motorcycle', 'Car', 'Bus', ' Train', 'Rikshaw', 'Chingchi', 'Cycle'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
---
+++ Ex.13 Your Own Array/Your_own_Array.ts
@@ -0,0 +1,4 @@
+ let transportation :string[] = ['Motorcycle', 'Car','Bus',' Train','Rikshaw','Chingchi','Cycle'] ;
+ for(let i=0; i<transportation.length; i++){
+    console.log('I would like to own a ' + transportation[i])
+ } 
