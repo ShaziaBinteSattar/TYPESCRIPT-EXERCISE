ee19b7a00a2aa9ddf7848141e421bc174d873558
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:26:54 +0500

    Add files via upload

---
+++ Ex.14 Guest List/Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Shumaila', 'Sana', 'Sobia', 'Mahnoor', 'Kiran'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dearest Ms. ' + Guest_List[i] + ',\n\n Its mine pleasure to invite you at mine dinner party. Its mine pleasure to have your prescence at this auspecious occasion. Please come and join us I will be waiting for you. \n\n\n THANK YOU \n\n\n With best Regards \n\n SHAZIA BINTE ABDUS SATTAR \n\n\n');
+}
---
+++ Ex.14 Guest List/Guest_List.ts
@@ -0,0 +1,5 @@
+let Guest_List :string[] = [ 'Shumaila', 'Sana', 'Sobia', 'Mahnoor', 'Kiran'];
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dearest Ms. '+Guest_List[i] +',\n\n Its mine pleasure to invite you at mine dinner party. Its mine pleasure to have your prescence at this auspecious occasion. Please come and join us I will be waiting for you. \n\n\n THANK YOU \n\n\n With best Regards \n\n SHAZIA BINTE ABDUS SATTAR \n\n\n')
+
+}
