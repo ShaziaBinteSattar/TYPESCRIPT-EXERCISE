a17a07bdd08ecee47d03504dd7576baa143570db
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-02-29 23:54:17 +0500

    Add files via upload

---
+++ Ex.2 Personal Message/app.ts
@@ -0,0 +1,9 @@
+let personName :string = '';
+personName = prompt('What is your Name?') ||'';
+if(personName !== null && personName !==''){
+alert(`Hello ${personName}, would you like to learn some Python today?`)
+}
+else {alert ('you have to fill your name!')
+}
+
+
