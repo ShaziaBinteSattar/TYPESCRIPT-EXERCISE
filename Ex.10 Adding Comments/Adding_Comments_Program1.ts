828c78bebd529af0ee0ecc0c014c0a209f449f14
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:15:57 +0500

    Add files via upload

---
+++ Ex.10 Adding Comments/Adding_Comments_Program1.ts
@@ -0,0 +1,6 @@
+//Program Written by : SHAZIA BINTE ABDUS SATTAR
+// Current Date : 23-02-2024
+// Current Day : Friday
+//This Program will writes my comments in the Program
+console.log('" This Program will writes my comments in the Program"');
+
---
+++ Ex.10 Adding Comments/Adding_Comments_Program2.ts
@@ -0,0 +1,11 @@
+//Program Written by : SHAZIA BINTE ABDUS SATTAR
+// Current Date : 23-02-2024
+// Current Day : Friday
+// This Program will add two digits.
+console.log(8+8);
+
+
+
+
+
+
