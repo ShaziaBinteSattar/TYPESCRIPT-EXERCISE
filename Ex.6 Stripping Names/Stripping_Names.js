e671477c0027f7409d98a19356298463a5c1d064
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:01:22 +0500

    Add files via upload

---
+++ Ex.6 Stripping Names/Stripping_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '   \n\tShazia Binte Abdus Sattar\t\n   ';
+console.log(personName);
+const without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ Ex.6 Stripping Names/Stripping_Names.ts
@@ -0,0 +1,10 @@
+const personName :string ='   \n\tShazia Binte Abdus Sattar\t\n   ';
+console.log (personName);
+const without_whitespace :string = personName.trim();
+console.log (without_whitespace);
+
+
+
+
+
+
