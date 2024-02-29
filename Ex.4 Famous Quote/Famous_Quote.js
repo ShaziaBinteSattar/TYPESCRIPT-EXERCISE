34cd16915ac5876fed5766fa713111e89025b4f2
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-02-29 23:58:14 +0500

    Add files via upload

---
+++ Ex.4 Famous Quote/Famous_Quote.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('Oprah Winfrey said, "Education is the key to unlocking the world.It is the pasport to freedom"');
---
+++ Ex.4 Famous Quote/Famous_Quote.ts
@@ -0,0 +1,4 @@
+console.log('Oprah Winfrey said, "Education is the key to unlocking the world.It is the pasport to freedom"');
+
+
+
