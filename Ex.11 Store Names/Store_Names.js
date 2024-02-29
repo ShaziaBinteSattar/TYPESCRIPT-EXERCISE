e92faa43c4b7d964fe7fe3e1a977cccb9f48a8a2
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:18:20 +0500

    Add files via upload

---
+++ Ex.11 Store Names/Store_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Rabia', 'Rabya', 'Qirat', 'Noor'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Ex.11 Store Names/Store_Names.ts
@@ -0,0 +1,4 @@
+let names :string[] = ['Rabia', 'Rabya', 'Qirat', 'Noor'] ;
+for( let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
