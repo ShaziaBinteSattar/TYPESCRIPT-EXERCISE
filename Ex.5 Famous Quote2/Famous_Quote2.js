106ceb151db8e424557164c35f3103fcd1b123ff
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-02-29 23:59:51 +0500

    Add files via upload

---
+++ Ex.5 Famous Quote2/Famous_Quote2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = ' Malcolm x';
+let message = '"Education is the passport to the future, for tomorrow belongs to those who prepare"';
+console.log(`${famous_person} once said,${message}`);
---
+++ Ex.5 Famous Quote2/Famous_Quote2.ts
@@ -0,0 +1,4 @@
+let famous_person :string =' Malcolm x';
+let message :string = '"Education is the passport to the future, for tomorrow belongs to those who prepare"';
+console.log(`${famous_person} once said,${message}`);
+
