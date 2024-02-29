b3123ea22a01cad282f93da79536476a9a01a755
shazia binte abdus sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 00:21:27 +0500

    Add files via upload

---
+++ Ex.12 Greetings/Store_Names_Greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Rabia', 'Rabya', 'Qirat', 'Noor'];
+const message = 'Welcome to typescript Class at Governor House in IT Initiative Program: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Ex.12 Greetings/Store_Names_Greetings.ts
@@ -0,0 +1,5 @@
+let names :string[] = ['Rabia', 'Rabya', 'Qirat', 'Noor'] ;
+const message :string = 'Welcome to typescript Class at Governor House in IT Initiative Program: '
+for( let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
