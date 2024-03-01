39ffa1b40e84224fc3901fbeef205ca0fc7a7f78
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-01 20:12:18 +0500

    Delete personal message directory

--- personal message/app.ts
+++
@@ -1,9 +0,0 @@
-let personName :string = '';
-personName = prompt('What is your Name?') ||'';
-if(personName !== null && personName !==''){
-alert(`Hello ${personName}, would you like to learn some Python today?`)
-}
-else {alert ('you have to fill your name!')
-}
-
-
