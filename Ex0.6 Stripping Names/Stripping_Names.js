e4d9f89ac8691aa27377d03f1c801a42e09015dd
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-08 13:40:17 +0500

    Delete Ex0.6 Stripping Names directory

--- Ex0.6 Stripping Names/Stripping_Names.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const personName = '   \n\tShazia Binte Abdus Sattar\t\n   ';
-console.log(personName);
-const without_whitespace = personName.trim();
-console.log(without_whitespace);
