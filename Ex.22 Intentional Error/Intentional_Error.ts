adf180f624e200b08e5fa47b49621574394475f4
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-13 13:45:13 +0500

    Add files via upload

---
+++ Ex.22 Intentional Error/Intentional_Error.ts
@@ -0,0 +1,17 @@
+//Ex: 22. Intentional Error: 
+//If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+//Creating Array//
+let newArray :string[] =['Hazarat Abu Bakar Siddique R.A' , 'Hazrat Umar Farooq R.A' , 'Hazrat Usman GHani R.A' , 'Hazrat Ali Murtaza R.A' ];
+
+//Change an index in one of your programs to produce an index error.//
+//Accessing Array by index//
+console.log(newArray[5]);
+
+
+
+//Make sure you correct the error before closing the program.// 
+//Correct the Array//
+console.log(newArray);
+
+
