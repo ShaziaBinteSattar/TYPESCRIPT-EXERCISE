0e33acbf1b0bce839e8fb5f68a117e9520a6969c
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-14 01:55:36 +0500

    Add files via upload

---
+++ Ex.25 Alien Colors1/Alien_Colors1.ts
@@ -0,0 +1,30 @@
+// Ex: 25. Alien Colors #1: 
+
+
+// • Imagine an alien was just shot down in a game.
+// • Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'. 
+
+//Create a variable called alien_color//
+let alien_color: string = 'green';
+
+
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the 
+// player just earned 5 points. 
+
+if (alien_color === 'green'){
+    console.log("Player just earned 5 points !");
+}
+
+// • Write one version of this program that passes the if test and another that fails. 
+alien_color = 'yellow';
+
+//The version that fails will have no output //
+    
+if (alien_color === 'green'){
+    console.log("Player just earned 5 points !");
+}
+
+
+
+
+
