425d4aee2304e1f0b2b4148e5691a8f4ae2928b7
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-13 13:42:40 +0500

    Add files via upload

---
+++ Ex.20 Empty Array/Empty_Array.js
@@ -0,0 +1,12 @@
+"use strict";
+//20. Ex: Empty Array
+//Think of something you could store in a array. For example, you could make a list of 
+// mountains, rivers, countries, cities, languages, or anything else you’d like.
+let item = ['Pakistan', 'Sindh', 'Karachi', 'Urdu'];
+console.log(item);
+// Write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+console.log(item2);
---
+++ Ex.20 Empty Array/Empty_Array.ts
@@ -0,0 +1,15 @@
+//20. Ex: Empty Array
+//Think of something you could store in a array. For example, you could make a list of 
+// mountains, rivers, countries, cities, languages, or anything else you’d like.
+let item :string[] = ['Pakistan','Sindh','Karachi','Urdu'];
+
+console.log(item);
+
+// Write a program that creates a list containing these items.
+let item2 :string[] = [];
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
+
