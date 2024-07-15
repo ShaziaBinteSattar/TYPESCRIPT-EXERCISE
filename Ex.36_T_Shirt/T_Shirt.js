5478c170afc7fcbaa61c098caba7e3f3d5f2c8fc
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-16 03:12:15 +0500

    Add files via upload

---
+++ Ex.36_T_Shirt/T_Shirt.js
@@ -0,0 +1,14 @@
+"use strict";
+//36. T-Shirt:
+//Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+//The function should print a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function. 
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message:${text}`);
+}
+//large size with text //
+make_shirt('Large', ' The secret of success is to know something nobody else knows');
+//Medium size with text //
+make_shirt('Medium', 'Believe in yourself');
+//Small size with text //
+make_shirt('Small', ' Great thing never came from comfort zones');
---
+++ Ex.36_T_Shirt/T_Shirt.ts
@@ -0,0 +1,16 @@
+//36. T-Shirt:
+//Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+//The function should print a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function. 
+
+function make_shirt(size : string, text: string){
+
+    console.log(`Creating a ${size} shirt with the message:${text}`);
+
+}
+//large size with text //
+make_shirt('Large' , ' The secret of success is to know something nobody else knows');
+//Medium size with text //
+make_shirt('Medium' , 'Believe in yourself');
+//Small size with text //
+make_shirt('Small' , ' Great thing never came from comfort zones');
