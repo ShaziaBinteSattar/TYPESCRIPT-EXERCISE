168ffc1624aff196746926b533f642b252a3c120
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-17 02:36:29 +0500

    Add files via upload

---
+++ Ex.44_Sandwiches/Sandwiches.js
@@ -0,0 +1,14 @@
+"use strict";
+//Ex: 44. Sandwiches:
+//Write a function that accepts a array of items a person wants on a sandwich. 
+//The function should have one parameter that collects as many items as the function call provides, 
+//and it should print a summary of the sandwich that is being ordered. 
+//Call the function three times, using a different number of arguments each time.
+function makeSandwich(item) {
+    console.log('\n\n Making your sandwich with:');
+    item.forEach(element => console.log(" - " + element));
+    console.log('Enjoy your Sandwich!');
+}
+makeSandwich(['Ham', 'Cheese', 'Lettuce']);
+makeSandwich(['Turkey', 'Cheese', 'Bacon']);
+makeSandwich(['Fried Egg', 'Cheese', 'Cucumber']);
